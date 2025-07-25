364ee2d410d886ad283f613217c2df01afed71f3
Erin Hollett <erinkhollett@gmail.com>
2025-07-25 14:52:56 -0400

    Added Focus Event

--- script.js
+++ script.js
@@ -26,3 +26,36 @@ document.getElementById('refresh').addEventListener('click', function() {
   input.value = '';
   output.textContent = "You're naming your pet:";
 })
+
+// === Form Event === //
+const form = document.getElementById('myForm');
+const submitMessage = document.getElementById('messageContainer');
+
+document.addEventListener('DOMContentLoaded', function() {
+  form.addEventListener('submit', function(event) {
+    console.log('Form submitted');
+    event.preventDefault(); // Prevent the actual form submission
+    submitMessage.textContent = "Submitted! Thank you for adopting " + input.value + "!";
+  });
+});
+
+// === Focus Event === //
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const nameFeedback = document.getElementById('nameFeedback');
+const emailFeedback = document.getElementById('emailFeedback');
+
+
+nameInput.addEventListener('focus', () => {
+  nameFeedback.textContent = "Please enter your full name.";
+});
+nameInput.addEventListener('blur', () => {
+  nameFeedback.textContent = "";
+});
+
+emailInput.addEventListener('focus', () => {
+  emailFeedback.textContent = "We won't share your email.";
+});
+emailInput.addEventListener('blur', () => {
+  emailFeedback.textContent = "";
+});
