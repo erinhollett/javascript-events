735c4934044b11f30fbd2d23de4cf4607172c0be
Erin Hollett <erinkhollett@gmail.com>
2025-07-25 14:20:57 -0400

    Added Keyboard Event

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const messageOnClick = document.getElementById('clickMessage');;
+const messageOnClick = document.getElementById('clickMessage');
+
 
 // === Mouse Event === //
 document.getElementById('clickButton').addEventListener('click', function() {
@@ -9,3 +10,19 @@ document.getElementById('clickButton').addEventListener('click', function() {
       messageOnClick.style.display = 'none';
   }, 3000);
 });
+
+
+// === Keyboard Event === //
+const input = document.getElementById('petName')
+const output = document.getElementById('showName');
+
+document.addEventListener('keypress', function(event) {
+  setTimeout(() => {
+    output.textContent = "You're naming your pet: " + input.value;
+  }, 0);
+});
+
+document.getElementById('refresh').addEventListener('click', function() {
+  input.value = '';
+  output.textContent = "You're naming your pet:";
+})
