499e3c36247d51a601947ebf9b3ac99a7801275d
Erin Hollett <erinkhollett@gmail.com>
2025-07-25 15:30:56 -0400

    Added petName placeholder function

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const messageOnClick = document.getElementById('clickMessage');
 
 
 // === Mouse Event === //
+const messageOnClick = document.getElementById('clickMessage');
+
 document.getElementById('clickButton').addEventListener('click', function() {
     messageOnClick.style.display = 'block';
 
-    // Timeout (something extra for fun!)
+    // EXTRA:
+    // Use timeout to make the message dissapear after 3 seconds (so the user can keep petting the pet!)
     setTimeout(() => {
       messageOnClick.style.display = 'none';
   }, 3000);
@@ -16,16 +18,29 @@ document.getElementById('clickButton').addEventListener('click', function() {
 const input = document.getElementById('petName')
 const output = document.getElementById('showName');
 
+// EXTRA:
+// Make the name the placeholder if nothing is entered
+function getPetName() {
+  const pName = input.value.trim(); // Trim spaces
+  if (pName === "") {
+  return input.placeholder;
+  } else {
+  return pName;
+  }
+}
+
+output.textContent = "You're naming your pet: " + getPetName();
+
 document.addEventListener('keypress', function(event) {
   setTimeout(() => {
-    output.textContent = "You're naming your pet: " + input.value;
+    output.textContent = "You're naming your pet: " + getPetName();
   }, 0);
 });
 
 document.getElementById('refresh').addEventListener('click', function() {
   input.value = '';
-  output.textContent = "You're naming your pet:";
-})
+  output.textContent = "You're naming your pet: " + getPetName();
+});
 
 // === Form Event === //
 const form = document.getElementById('myForm');
@@ -35,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
   form.addEventListener('submit', function(event) {
     console.log('Form submitted');
     event.preventDefault(); // Prevent the actual form submission
-    submitMessage.textContent = "Submitted! Thank you for adopting " + input.value + "!";
+    submitMessage.textContent = "Submitted! Thank you for adopting " + getPetName() + "!";
   });
 });
 
@@ -65,7 +80,7 @@ const list = document.getElementById('toyBox');
 
 list.addEventListener('click', function(event) {
   if (event.target.tagName === 'LI') {
-      submitMessage.textContent = "You gave: " + input.value + "a " + event.target.textContent;
+      submitMessage.textContent = "You gave: " + getPetName() + " a " + event.target.textContent;
       event.target.style.backgroundColor = 'green';
   }
 });
